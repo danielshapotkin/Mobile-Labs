4e4403148f84d1dac93f8d126f639b9866428044
danielshapotkin <Daniel.Shapotkin@solbeg.com>
2024-09-14 12:30:59 +0300

    Add files via upload

---
+++ Lab23/app/src/main/java/com/example/lab23/LayoutActivity.kt
@@ -0,0 +1,14 @@
+package com.example.lab23
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout)
+
+
+
+    }
+}
